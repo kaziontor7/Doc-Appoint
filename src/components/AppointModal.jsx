2815c9ffefa91785432aa5d4dbfb4d7283765322
kaziontor7 <kaziontor7@gmail.com>
2026-05-20 19:03:33 +0600

    added profile and logout

--- src/components/AppointModal.jsx
+++ src/components/AppointModal.jsx
@@ -1,29 +1,43 @@
 "use client";
+
+import { authClient } from "@/lib/auth-client";
 import { Envelope } from "@gravity-ui/icons";
 import { Button, Input, Label, Modal, Surface, TextField, ListBox, Select, FieldError } from "@heroui/react";
 import { CgNotes } from "react-icons/cg";
 import { FaRegCalendarAlt } from "react-icons/fa";
 
-const AppointModal = ({doctor}) => {
+const AppointModal = ({ doctor }) => {
     console.log(doctor);
+    const userData = authClient.useSession()
+    const user = userData?.data?.user
+    console.log(user);
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        const formData = new FormData(e.target);
+        const values = Object.fromEntries(formData.entries());
+        console.log(values);
+
+    }
     return (
         <div  >
             <Modal className={' p-0 m-0'}>
                 <Button className={'text-white font-semibold text-lg bg-[#004AC6] rounded-2xl'}><FaRegCalendarAlt />
                     Book Appointment</Button>
                 <Modal.Backdrop >
-                    <Modal.Container placement="auto"  className={'p-0 m-0'}>
+                    <Modal.Container placement="auto" className={'p-0 m-0'}>
                         <Modal.Dialog className="sm:max-w-md ">
-                             <Modal.CloseTrigger className="mt-3"/>
+                            <Modal.CloseTrigger className="mt-3" />
                             <Modal.Header className="">
-                               <div className="flex items-center gap-2">
-                  
+                                <div className="flex items-center gap-2">
+
                                     <Modal.Icon className="bg-accent-soft text-accent-soft-foreground">
                                         <CgNotes
                                             className="size-5" />
                                     </Modal.Icon>
                                     <Modal.Heading className="title text-xl">Book Appointment</Modal.Heading>
-                                    
+
                                 </div>
                                 <p className="mt-1.5 text-sm leading-5 text-muted">
                                     Fill out the form below and we&apos;ll get back to you.
@@ -31,8 +45,8 @@ const AppointModal = ({doctor}) => {
                             </Modal.Header>
                             <Modal.Body className="p-6">
                                 <Surface variant="default">
-                                    <form className="flex flex-col gap-4">
-                                        <TextField  className="w-full " name="name" type="text" variant="secondary">
+                                    <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+                                        <TextField className="w-full " name="doctorName" type="text" variant="secondary">
                                             <Label>Doctor Name</Label>
                                             <Input value={doctor.name} className={'bg-[#edf3ff] border border-[#C3C6D7]'} disabled placeholder="Enter Doctor Name" />
                                         </TextField>
@@ -43,10 +57,10 @@ const AppointModal = ({doctor}) => {
                                         </TextField>
                                         <TextField isRequired className="w-full" name="phone" type="tel" variant="secondary">
                                             <Label>Phone</Label>
-                                            <Input className={'bg-white border border-[#C3C6D7]'}  placeholder="Enter your phone number" />
+                                            <Input className={'bg-white border border-[#C3C6D7]'} placeholder="Enter your phone number" />
                                             <FieldError />
                                         </TextField>
-                                        <Select isRequired className="w-full  " placeholder="Select one">
+                                        <Select name="session" isRequired className="w-full  " placeholder="Select one">
                                             <Label>Session</Label>
                                             <Select.Trigger className={'border border-[#C3C6D7]'}>
                                                 <Select.Value />
@@ -63,19 +77,19 @@ const AppointModal = ({doctor}) => {
                                             </Select.Popover>
                                             <FieldError />
                                         </Select>
-                                         <Modal.Footer>
-                                
-                                        <Button slot="close" variant="secondary">
-                                    Cancel
-                                </Button>
-                                <Button type="submit" >
-                                    Book
-                                </Button>
-                            </Modal.Footer>
+                                        <Modal.Footer>
+
+                                            <Button slot="close" variant="secondary">
+                                                Cancel
+                                            </Button>
+                                            <Button type="submit" >
+                                                Book
+                                            </Button>
+                                        </Modal.Footer>
                                     </form>
                                 </Surface>
                             </Modal.Body>
-                           
+
                         </Modal.Dialog>
                     </Modal.Container>
                 </Modal.Backdrop>
