53bd36d1cba9bc58c08ad93fbb2925157438df57
kaziontor7 <kaziontor7@gmail.com>
2026-05-22 15:49:45 +0600

    improved search functionality

--- src/app/appointments/page.jsx
+++ src/app/appointments/page.jsx
@@ -1,24 +1,36 @@
 'use client'
 import DocCard from "@/components/DocCard";
 import {  doctorsPromise } from "@/data";
-import { SearchField, Surface} from "@heroui/react";
+import { Button, SearchField, Surface} from "@heroui/react";
 import { use, useState } from "react";
 const doctorsPromised = doctorsPromise(`${process.env.NEXT_PUBLIC_SERVER_URL}/appoints`);
 
 
 const Appointments =  () => {
     const [query, setQuery] = useState(doctorsPromised);
+    const [searchValue, setSearchValue] = useState(null)
      const doctors = use(query);
-     const searchHandler = (e) => {
+     const queryHandler = (e) => {
        const value = e.target.value;
-       const searchPromise = doctorsPromise(`${process.env.NEXT_PUBLIC_SERVER_URL}/appoints/search/${value}`);
-       if(value.length === 0){
+      
+       if(value.length===0){
+        setSearchValue(null)
+       }
+       else{
+        setSearchValue(value)
+       }
+       
+    } 
+    const searchHandler =()=>{
+         const searchPromise = doctorsPromise(`${process.env.NEXT_PUBLIC_SERVER_URL}/appoints/search/${searchValue}`);
+
+        if(searchValue === null){
         setQuery(doctorsPromised);
        }
        else{
         setQuery(searchPromise);
        }
-    } 
+    }
     
     
     return (
@@ -26,19 +38,26 @@ const Appointments =  () => {
             <h2 className="title text-3xl">Find Your Specialist</h2>
             <p className="text text-lg">Search and book appointments with top medical professionals.</p>
             <div className="mt-6 mb-7">
-                <Surface className="flex w-full  flex-col gap-4 rounded-3xl p-3 border border-[#C3C6D7] ">
-      <SearchField name="search" variant="secondary"  >
+                <Surface className="flex w-full   gap-4 rounded-3xl p-3 border border-[#C3C6D7] flex-row items-center">
+      <SearchField name="search" variant="secondary" className={'w-full'} >
         <SearchField.Group className={'bg-[#f8f9ff] border border-[#C3C6D7] rounded-2xl'}>
           <SearchField.SearchIcon />
-          <SearchField.Input   className="w-full " placeholder="Search by Doctor Name" onChange={searchHandler} />
-          <SearchField.ClearButton />
+          <SearchField.Input onKeyDown={(e) => e.key === 'Enter' && searchHandler()}  className="w-full " placeholder="Search by Doctor Name" onChange={queryHandler} />
+          <SearchField.ClearButton onClick={()=>{setSearchValue(null);
+               setQuery(doctorsPromised)
+          }} />
         </SearchField.Group>
-        
+       
       </SearchField>
+       <Button onClick={searchHandler} variant="outline" className={'text-white border border-[#F8F9FF] rounded-3xl  bg-[#004AC6]'}>Search</Button>
       </Surface>
             </div>
             <div className="grid grid-cols-1 md:grid-cols-3 gap-6 pt-6">
-                {doctors.map(doctor => <DocCard key={doctor._id} doctor={doctor}></DocCard>)}
+               {doctors.length > 0 ? (
+                    doctors.map(doctor => <DocCard key={doctor._id} doctor={doctor} />)
+                ) : (
+                    <p className="col-span-full text-center text-gray-500">No doctors found matching your search.</p>
+                )}
             </div>
         </div>
     );
